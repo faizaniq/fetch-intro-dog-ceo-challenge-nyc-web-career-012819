e9e6f9782e4f51a8b59cb528ccbd1cda5ea0f1a4
faizaniq <github email address>
2019-03-14 18:22:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('%c HI', 'color: firebrick')
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  let dogs = []
+  let dogContainer = document.querySelector('#dog-image-container')
+  let breedList = document.querySelector('#dog-breeds')
+  let dropDown = document.querySelector('#breed-dropdown')
+
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(function(json) {
+    json.message.forEach(dog => {
+      dogContainer.innerHTML += (`<img src=${dog}>`)
+    })
+  })
+
+  fetch(breedUrl)
+  .then(res => res.json())
+  .then(function(json) {
+    for(keys in json.message) {
+      dogs.push(keys)
+      breedList.innerHTML += (`<li>${keys}</li>`)
+    }
+  })
+
+  document.addEventListener('click', (e) => {
+    if (dogs.includes(e.target.textContent)) {
+      e.target.style.color = 'yellow'
+      e.target.style.background = 'black'
+    }
+  })
+
+  dropDown.addEventListener('change', (e) => {
+    breedList.innerHTML = ('');
+    let input = e.target.value
+
+
+  dogs.filter(breed => {
+    if (breed[0] === input) {
+      breedList.innerHTML += (`<li>${breed}</li>`)
+      }
+    })
+  })
+})
